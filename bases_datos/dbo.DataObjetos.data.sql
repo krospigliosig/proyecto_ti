214a4bab562153d59263024ddfa4696187dcf00f
krospigliosig <132178872+krospigliosig@users.noreply.github.com>
2023-08-09 18:41:33 -0500

    Add files via upload

---
+++ bases_datos/dbo.DataObjetos.data.sql
@@ -0,0 +1,9 @@
+SET IDENTITY_INSERT [dbo].[DataObjetos] ON
+INSERT INTO [dbo].[DataObjetos] ([Id], [Objeto], [Deporte], [Cantidad], [Observacion], [Estado]) VALUES (6, N'Net', N'Ping Pong', 1, NULL, 1)
+INSERT INTO [dbo].[DataObjetos] ([Id], [Objeto], [Deporte], [Cantidad], [Observacion], [Estado]) VALUES (7, N'Tablero', N'Ajedrez', 1, N'', 1)
+INSERT INTO [dbo].[DataObjetos] ([Id], [Objeto], [Deporte], [Cantidad], [Observacion], [Estado]) VALUES (16, N'Pelota 5', N'Futbol', 5, N'2 desinfladas', 1)
+INSERT INTO [dbo].[DataObjetos] ([Id], [Objeto], [Deporte], [Cantidad], [Observacion], [Estado]) VALUES (17, N'Pelota 6', N'Basket', 0, N'Desinflada', 0)
+INSERT INTO [dbo].[DataObjetos] ([Id], [Objeto], [Deporte], [Cantidad], [Observacion], [Estado]) VALUES (18, N'Tablero', N'Ajedrez', 1, N'', 1)
+INSERT INTO [dbo].[DataObjetos] ([Id], [Objeto], [Deporte], [Cantidad], [Observacion], [Estado]) VALUES (19, N'Pelotita', N'Ping Pong', 48, N'2 malogradas', 1)
+INSERT INTO [dbo].[DataObjetos] ([Id], [Objeto], [Deporte], [Cantidad], [Observacion], [Estado]) VALUES (25, N'Mesa', N'Ping Pong', 1, N'', 1)
+SET IDENTITY_INSERT [dbo].[DataObjetos] OFF
